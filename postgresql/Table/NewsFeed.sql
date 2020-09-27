a556c9fbc22f9860d8f5db395e4b65ae6e6310fa
Cuates <48161526+Cuates@users.noreply.github.com>
2020-09-27 15:58:46 -0700

    Modified Table Schema
    Modified table schema

--- postgresql/Table/NewsFeed.sql
+++ postgresql/Table/NewsFeed.sql
@@ -1,6 +1,18 @@
 -- Database Connect
 \c <databasename>;
 
+-- =======================
+--        File: NewsFeed
+--     Created: 09/07/2020
+--     Updated: 09/27/2020
+--  Programmer: Cuates
+--   Update By: Cuates
+--     Purpose: News Feed
+-- =======================
+
+-- Table Drop
+drop table if exists NewsFeed;
+
 -- Sequence Create
 create sequence NewsFeed_nfID_seq;
 
@@ -9,7 +21,7 @@ create table if not exists NewsFeed(
   nfID bigint not null default nextval('NewsFeed_nfID_seq'),
   title varchar(255) not null,
   imageurl varchar(255) default null,
-  feedurl text default null,
+  feedurl varchar(768) not null,
   actualurl varchar(255) default null,
   publish_date timestamp not null,
   created_date timestamp not null default current_timestamp,
--- postgresql/Table/NewsFeedTemp.sql
+++ postgresql/Table/NewsFeedTemp.sql
@@ -1,6 +1,18 @@
 -- Database Connect
 \c <databasename>;
 
+-- ===========================
+--        File: NewsFeedTemp
+--     Created: 09/07/2020
+--     Updated: 09/27/2020
+--  Programmer: Cuates
+--   Update By: Cuates
+--     Purpose: News Feed Temp
+-- ===========================
+
+-- Table Drop
+drop table if exists NewsFeedTemp;
+
 -- Table Create
 create table if not exists NewsFeedTemp(
   title text default null,
