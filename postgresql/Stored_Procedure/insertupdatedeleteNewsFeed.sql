86e5cf4e012cb0a0245550299c76eaba0116fca1
Cuates <48161526+Cuates@users.noreply.github.com>
2020-09-27 15:54:01 -0700

    Modified Stored Procedure Logic
    Modified stored procedure logic

--- postgresql/Stored_Procedure/insertupdatedeleteNewsFeed.sql
+++ postgresql/Stored_Procedure/insertupdatedeleteNewsFeed.sql
@@ -1,11 +1,20 @@
 -- Database Connect
 \c <databasename>;
 
+-- ===========================================
+--        File: insertupdatedeleteNewsFeed
+--     Created: 09/07/2020
+--     Updated: 09/27/2020
+--  Programmer: Cuates
+--   Update By: Cuates
+--     Purpose: Insert Update Delete News Feed
+-- ===========================================
+
 -- Procedure Drop
 drop procedure if exists insertupdatedeleteNewsFeed;
 
--- Procedure Create
-create procedure insertupdatedeleteNewsFeed(in optionMode text, in title text default null, in imageurl text default null, in feedurl text default null, in actualurl text default null, in publishDate text default null, inout status text default null)
+-- Procedure Create Or Replace
+create or replace procedure insertupdatedeleteNewsFeed(in optionMode text, in title text default null, in imageurl text default null, in feedurl text default null, in actualurl text default null, in publishDate text default null, inout status text default null)
 as $$
   begin
     -- Omit characters, multi space to single space, and trim leading and trailing spaces
@@ -53,8 +62,8 @@ as $$
       feedurl := nullif(feedurl, '');
     end if;
 
-    -- -- Set character limit
-    -- feedurl := substring(feedurl, 1, 255);
+    -- Set character limit
+    feedurl := substring(feedurl, 1, 768);
 
     -- Omit characters, multi space to single space, and trim leading and trailing spaces
     actualurl := trim(regexp_replace(regexp_replace(actualurl, '[^a-zA-Z0-9 !"\#$%&''()*+,\-./:;<=>?@\[\\\]^_‘{|}~¡¢£¥¦§¨©®¯°±´µ¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿıŒœŠšŸŽžƒˆˇ˘˙˚˛ΓΘΣΦΩαδεπστφ–—‘’“”•…€™∂∆∏∑∙√∞∩∫≈≠≡≤≥]', ' '), '[ ]{2,}', ' '));
@@ -99,40 +108,86 @@ as $$
     elseif optionMode = 'insertTempNews' then
       -- Check if parameters are not null
       if title is not null and publishDate is not null then
-          -- Insert record
-          insert into NewsFeedTemp(title, imageurl, feedurl, actualurl, publish_date, created_date) values (title, imageurl, feedurl, actualurl, publishDate, current_timestamp);
+        -- Insert record
+        insert into NewsFeedTemp(title, imageurl, feedurl, actualurl, publish_date, created_date) values (title, imageurl, feedurl, actualurl, publishDate, current_timestamp);
 
-          -- Select message
-          select
-          'Success~Record(s) inserted' into status;
+        -- Select message
+        select
+        'Success~Record(s) inserted' into status;
       else
-          -- Select message
-          select
-          'Error~Process halted, title and or publish date were not provided' into status;
+        -- Select message
+        select
+        'Error~Process halted, title and or publish date were not provided' into status;
       end if;
 
     -- Else check if option mode is update bulk news
     elseif optionMode = 'updateBulkNews' then
+      -- Remove duplicate records based on group by
+      with subNewsDetails as
+      (
+        -- Select unique records
+        select
+        trim(nft.title) as nfttitle,
+        lower(trim(nft.title)) as nfttitlelower,
+        trim(nft.imageurl) as nftimageurl,
+        trim(nft.feedurl) as nftfeedurl,
+        trim(nft.actualurl) as nftactualurl,
+        nft.publish_date as nftpublishdate,
+        nf.nfID as nfnfID,
+        trim(nf.title) as nftitle,
+        lower(trim(nf.title)) as nftitlelower
+        from NewsFeedTemp nft
+        left join NewsFeed nf on nf.feedurl = nft.feedurl
+        where
+        (
+          nft.title is not null and
+          nft.feedurl is not null and
+          nft.publish_date is not null
+        ) -- and
+        -- (
+        --   cast(nft.publish_date as timestamp) >= current_timestamp + interval '-1 hour' and
+        --   cast(nft.publish_date as timestamp) <= current_timestamp + interval '0 hour'
+        -- )
+        group by nft.title, nft.imageurl, nft.feedurl, nft.actualurl, nft.publish_date, nf.title, nf.nfID, nf.title
+      ),
+      filteredNewsDetails as
+      (
+        -- Select unique records
+        select
+        snd.nfttitlelower as titlelower,
+        max(snd.nftpublishdate) as publishdate
+        from subNewsDetails snd
+        group by snd.nfttitlelower
+      ),
+      newsDetails as
+      (
+        -- Select unique records
+        select
+        snd.nfttitle as title,
+        snd.nftimageurl as imageurl,
+        snd.nftfeedurl as feedurl,
+        snd.nftactualurl as actualurl,
+        snd.nftpublishdate as publishdate,
+        snd.nfnfID as nfID
+        from subNewsDetails snd
+        join filteredNewsDetails fnd on fnd.titlelower = snd.nfttitlelower and fnd.publishdate = snd.nftpublishdate
+        where
+        (
+          snd.nfttitlelower = snd.nftitlelower
+        )
+        group by snd.nfttitle, snd.nftimageurl, snd.nftfeedurl, snd.nftactualurl, snd.nftpublishdate, snd.nfnfID
+      )
+
       -- Update records
       update NewsFeed
       set
-      imageurl = case when trim(nft.imageurl) = '' then null else trim(nft.imageurl) end,
-      feedurl = case when trim(nft.feedurl) = '' then null else trim(nft.feedurl) end,
-      actualurl = case when trim(nft.actualurl) = '' then null else trim(nft.actualurl) end,
-      publish_date = cast(nft.publish_date as timestamp),
+      imageurl = case when trim(nd.imageurl) = '' then null else trim(nd.imageurl) end,
+      feedurl = nd.feedurl,
+      actualurl = case when trim(nd.actualurl) = '' then null else trim(nd.actualurl) end,
+      publish_date = cast(nd.publishdate as timestamp),
       modified_date = current_timestamp
-      from NewsFeedTemp nft
-      left join NewsFeed nf on nf.title = nft.title
-      where
-      (
-        nft.title is not null and
-        nft.publish_date is not null
-      ) and
-      (
-        cast(nft.publish_date as timestamp) >= current_timestamp + interval '-1 hour' and
-        cast(nft.publish_date as timestamp) <= current_timestamp + interval '0 hour'
-      ) and
-      nf.title is not null;
+      from newsDetails nd
+      join NewsFeed nf on nf.nfID = nd.nfID;
 
       -- Select message
       select
@@ -144,40 +199,88 @@ as $$
       insert into newsfeed (title, imageurl, feedurl, actualurl, publish_date, created_date, modified_date)
 
       -- Remove duplicate records based on group by
-      with newsDetails as
+      with subNewsDetails as
       (
         -- Select unique records
         select
-        distinct
-        nft.title as title,
-        max(nft.publish_date) as publish_date
+        trim(nft.title) as nfttitle,
+        lower(trim(nft.title)) as nfttitlelower,
+        trim(nft.imageurl) as nftimageurl,
+        trim(nft.feedurl) as nftfeedurl,
+        trim(nft.actualurl) as nftactualurl,
+        nft.publish_date as nftpublishdate,
+        nf.nfID as nfnfID,
+        trim(nf.title) as nftitle,
+        lower(trim(nf.title)) as nftitlelower
         from NewsFeedTemp nft
-        group by nft.title
+        left join NewsFeed nf on nf.feedurl = nft.feedurl
+        where
+        (
+          nft.title is not null and
+          nft.feedurl is not null and
+          nft.publish_date is not null
+        ) -- and
+        -- (
+        --   cast(nft.publish_date as timestamp) >= current_timestamp + interval '-1 hour' and
+        --   cast(nft.publish_date as timestamp) <= current_timestamp + interval '0 hour'
+        -- )
+        group by nft.title, nft.imageurl, nft.feedurl, nft.actualurl, nft.publish_date, nf.title, nf.nfID, nf.title
+      ),
+      filteredNewsDetails as
+      (
+        -- Select unique records
+        select
+        snd.nfttitlelower as titlelower,
+        max(snd.nftpublishdate) as publishdate
+        from subNewsDetails snd
+        group by snd.nfttitlelower
+      ),
+      newsDetails as
+      (
+        -- Select unique records
+        select
+        snd.nfttitle as title,
+        snd.nftimageurl as imageurl,
+        snd.nftfeedurl as feedurl,
+        snd.nftactualurl as actualurl,
+        snd.nftpublishdate as publishdate,
+        snd.nfnfID as nfID
+        from subNewsDetails snd
+        join filteredNewsDetails fnd on fnd.titlelower = snd.nfttitlelower and fnd.publishdate = snd.nftpublishdate
+        where
+        (
+          snd.nfttitlelower <> snd.nftitlelower or
+          snd.nftitle is null
+        )
+        group by snd.nfttitle, snd.nftimageurl, snd.nftfeedurl, snd.nftactualurl, snd.nftpublishdate, snd.nfnfID
       )
 
       -- Select records
       select
-      distinct
-      trim(nft.title),
-      case when trim(nft.imageurl) = '' then null else trim(nft.imageurl) end,
-      case when trim(nft.feedurl) = '' then null else trim(nft.feedurl) end,
-      case when trim(nft.actualurl) = '' then null else trim(nft.actualurl) end,
-      cast(nft.publish_date as timestamp),
+      nd.title,
+      case
+        when trim(nd.imageurl) = ''
+          then
+            null
+        else
+          trim(nd.imageurl)
+      end,
+      nd.feedurl,
+      case
+        when trim(nd.actualurl) = ''
+          then
+            null
+        else
+          trim(nd.actualurl)
+      end,
+      cast(nd.publishdate as timestamp),
       current_timestamp,
       current_timestamp
       from newsDetails nd
-      join NewsFeedTemp nft on nft.title = nd.title and nft.publish_date = nd.publish_date
-      left join NewsFeed nf on nf.title = nft.title
+      left join NewsFeed nf on nf.title = nd.title
       where
-      (
-        nft.title is not null and
-        nft.publish_date is not null
-      ) and
-      -- (
-        -- cast(nft.publish_date as timestamp) >= current_timestamp + interval '-1 hour' and
-        -- cast(nft.publish_date as timestamp) <= current_timestamp + interval '0 hour'
-      -- ) and
-      nf.title is null;
+      nf.title is null
+      group by nd.title, nd.imageurl, nd.feedurl, nd.actualurl, nd.publishdate;
 
       -- Select message
       select
