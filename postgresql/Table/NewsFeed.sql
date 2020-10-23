e06ccc24150365244c994366c7d59cd6eb33de51
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-23 10:19:54 -0700

    Update NewsFeed.sql

--- postgresql/Table/NewsFeed.sql
+++ postgresql/Table/NewsFeed.sql
@@ -2,23 +2,23 @@
 \c <databasename>;
 
 -- =======================
---        File: NewsFeed
+--        File: newsfeed
 --     Created: 09/07/2020
---     Updated: 10/05/2020
+--     Updated: 10/23/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: News Feed
 -- =======================
 
 -- Table Drop
-drop table if exists NewsFeed;
+drop table if exists newsfeed;
 
 -- Sequence Create
-create sequence NewsFeed_nfID_seq;
+create sequence newsfeed_nfID_seq;
 
 -- Table Create
-create table if not exists NewsFeed(
-  nfID bigint not null default nextval('NewsFeed_nfID_seq'),
+create table if not exists newsfeed(
+  nfID bigint not null default nextval('newsfeed_nfID_seq'),
   title citext not null,
   imageurl citext default null,
   feedurl citext not null,
@@ -26,11 +26,11 @@ create table if not exists NewsFeed(
   publish_date timestamp not null,
   created_date timestamp not null default current_timestamp,
   modified_date timestamp default current_timestamp,
-  constraint PK_NewsFeed_title primary key (title)
+  constraint pk_newsfeed_title primary key (title)
 );
 
 -- Sequence Alter ownership
-alter sequence NewsFeed_nfID_seq owned by NewsFeed.nfID;
+alter sequence newsfeed_nfID_seq owned by newsfeed.nfID;
 
 -- Grant permission to a sequence
-grant usage, select on sequence NewsFeed_nfID_seq to <username>;
+grant usage, select on sequence newsfeed_nfID_seq to <username>;
